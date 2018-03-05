2725cbbb588f53a8d150e21e776980c2e172364c
airtoxin <airtoxin@icloud.com>
2018-03-06 08:19:09 +0900

    Add files

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const cssnext = require("postcss-cssnext");
+
+if (!process.env.NODE_ENV)
+  throw new Error("Could not find required environment variable NODE_ENV");
+
+const outPath = path.join(__dirname, "build");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    //  出力ファイルのディレクトリ名
+    path: outPath,
+    // 出力ファイル名
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              namedExport: true,
+              importLoaders: 1,
+              localIdentName: "[path][name]__[local]--[hash:base64:5]",
+              minimize: {
+                autoprefixer: false
+              }
+            }
+          },
+          "postcss-loader"
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"]
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: "node_modules/semantic-ui-css/semantic.min.css",
+        to: `${outPath}/assets/`,
+        flatten: true
+      }
+    ]),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "src/index.html",
+      hash: true,
+      minify: {
+        collapseWhitespace: true
+      }
+    })
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "build"),
+    port: 9000,
+    historyApiFallback: true
+  }
+};
