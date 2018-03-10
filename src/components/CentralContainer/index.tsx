fbd2e675f8e49a19d6600e1e8fd9a9adf88a1e84
airtoxin <airtoxin@icloud.com>
2018-03-10 18:06:35 +0900

    Add central container component

---
+++ src/components/CentralContainer/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import * as styles from './styles.css';
+
+export class CentralContainer extends React.Component {
+  render() {
+    return (
+      <div className={styles.verticalCenter}>
+        <div className={styles.horizontalCenter}>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
