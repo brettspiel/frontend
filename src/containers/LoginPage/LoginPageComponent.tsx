51b7f9af6e9cbf1bd5c62cfd0fbb44eb4fc5a4f8
airtoxin <airtoxin@icloud.com>
2018-03-10 18:49:09 +0900

    Cleanup views

--- src/containers/LoginPage/LoginPageComponent.tsx
+++ src/containers/LoginPage/LoginPageComponent.tsx
@@ -1,12 +1,33 @@
 import * as React from "react";
+import {CentralContainer} from '../../components/CentralContainer';
+import {Form, Divider} from "semantic-ui-react";
+import * as styles from './styles.css';
 
 export class LoginPageComponent extends React.Component {
   render() {
     return (
-      <div>
-        <label>ユーザー名</label>
-        <input type="text" />
-      </div>
+      <CentralContainer>
+        <Form className={styles.form}>
+          <Form.Field>
+            <label>ユーザー名</label>
+            <Form.Input type="text"/>
+          </Form.Field>
+
+          <Divider />
+
+          <Form.Field>
+            <label>サーバーホスト名</label>
+            <Form.Input type="text"/>
+          </Form.Field>
+
+          <Form.Field>
+            <label>サーバーポート番号</label>
+            <Form.Input type="text"/>
+          </Form.Field>
+
+          <Form.Button>接続</Form.Button>
+        </Form>
+      </CentralContainer>
     );
   }
 }
