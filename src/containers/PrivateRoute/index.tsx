c0de7be8dc97c46589093a42e04cc18c60e00eae
airtoxin <airtoxin@icloud.com>
2018-03-11 23:30:23 +0900

    fmt

--- src/containers/PrivateRoute/index.tsx
+++ src/containers/PrivateRoute/index.tsx
@@ -1,32 +1,31 @@
-import * as React from 'react';
-import {UserState} from "../../modules/user";
-import {ServerState} from "../../modules/server";
-import {connect} from "react-redux";
-import {StoreState} from "../../modules";
-import {Route, RouteProps} from "react-router-dom";
-import {Redirect, RouteComponentProps} from "react-router";
-import {User} from "../../domain/models/User";
-import {Overwrite} from "utility-types";
+import * as React from "react";
+import { UserState } from "../../modules/user";
+import { ServerState } from "../../modules/server";
+import { connect } from "react-redux";
+import { StoreState } from "../../modules";
+import { Route, RouteProps } from "react-router-dom";
+import { Redirect, RouteComponentProps } from "react-router";
+import { User } from "../../domain/models/User";
+import { Overwrite } from "utility-types";
 
 export interface InjectedProps {
-  user: UserState,
-  server: ServerState,
+  user: UserState;
+  server: ServerState;
 }
 
 export interface HijackedProps {
-  render: ((props: RouteComponentProps<any> & {user: User}) => React.ReactNode)
+  render: ((
+    props: RouteComponentProps<any> & { user: User }
+  ) => React.ReactNode);
 }
 
 export type ExternalProps = Overwrite<RouteProps, HijackedProps>;
 
-export class PrivateRouteHoC extends React.Component<InjectedProps & ExternalProps> {
+export class PrivateRouteHoC extends React.Component<
+  InjectedProps & ExternalProps
+> {
   render() {
-    const {
-      user,
-      server,
-      render,
-      ...otherProps
-    } = this.props;
+    const { user, server, render, ...otherProps } = this.props;
 
     if (!user.id || !user.name) {
       return <Redirect to="/login" />;
@@ -37,7 +36,7 @@ export class PrivateRouteHoC extends React.Component<InjectedProps & ExternalPro
 
     const userModel: User = {
       id: user.id,
-      name: user.name,
+      name: user.name
     };
 
     return (
