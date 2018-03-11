a2253ad8a7093875c10ac5db6445ec6b500538f0
airtoxin <airtoxin@icloud.com>
2018-03-12 01:11:56 +0900

    Add counter repository

---
+++ src/domain/repositories/CounterRepository.ts
@@ -0,0 +1,26 @@
+import * as SocketIo from 'socket.io-client';
+
+export class CounterRepository {
+  private socket: SocketIOClient.Socket;
+  constructor(private serverUrl: string) {
+    this.socket = SocketIo(`${this.serverUrl}`, { path: 'counter' });
+  }
+
+  get(): Promise<number> {
+    return new Promise(resolve => {
+      this.socket.emit("get");
+      this.socket.once("update", (count: number) => {
+        resolve(count);
+      });
+    });
+  }
+
+  add(delta: number): Promise<number> {
+    return new Promise(resolve => {
+      this.socket.emit("patch", delta);
+      this.socket.once("update", (count: number) => {
+        resolve(count);
+      });
+    });
+  }
+}
