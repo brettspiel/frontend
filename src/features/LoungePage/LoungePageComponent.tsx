551b744de9604a66dade39f8dc40b4f1bfcf38cf
airtoxin <airtoxin@icloud.com>
2018-03-24 08:48:11 +0900

    Add chat component

--- src/features/LoungePage/LoungePageComponent.tsx
+++ src/features/LoungePage/LoungePageComponent.tsx
@@ -1,12 +1,8 @@
 import * as React from "react";
 import { User } from "../../domain/models/User";
 import { LoungePagePresenter } from "./LoungePagePresenter";
-import { DraggableCore } from "react-draggable";
-import { withFormik } from "formik";
-import { Form, Divider, Feed } from "semantic-ui-react";
-import * as styles from "./styles.css";
 import { ChatMessage } from "../../domain/models/ChatMessage";
-import { reverse } from "lodash";
+import { Chat } from "../../components/Chat";
 
 export interface Props {
   user: User;
@@ -18,36 +14,7 @@ export interface FormProps {
   presenter: LoungePagePresenter;
 }
 
-export interface State {
-  heightDiff: number;
-}
-
-export interface ChatSendFormValue {
-  message: string;
-}
-
-const ChatSendForm = withFormik<Props, ChatSendFormValue>({
-  mapPropsToValues: () => ({ message: "" }),
-  handleSubmit: async (values, { props, resetForm }) => {
-    await props.presenter.handleSubmitChatMessage(values);
-    resetForm();
-  }
-})(({ values, handleChange, handleSubmit }) => (
-  <Form onSubmit={handleSubmit}>
-    <Form.Input
-      name="message"
-      fluid
-      action="送信"
-      placeholder="Hi, ..."
-      value={values.message}
-      onChange={handleChange}
-    />
-  </Form>
-));
-
-const defaultChatAreaHeight = "5em";
-
-export class LoungePageComponent extends React.Component<Props, State> {
+export class LoungePageComponent extends React.Component<Props> {
   constructor(props: Props) {
     super(props);
     this.state = {
@@ -57,45 +24,14 @@ export class LoungePageComponent extends React.Component<Props, State> {
 
   render() {
     return (
-      <div className={styles.container}>
-        <div className={styles.mainArea}>
-          <h1>{this.props.user.name}</h1>
-        </div>
-        <DraggableCore
-          onDrag={(_e, data) =>
-            this.setState({ heightDiff: this.state.heightDiff + data.deltaY })
-          }
-        >
-          <Divider horizontal className={styles.divider}>
-            ラウンジチャット
-          </Divider>
-        </DraggableCore>
-        <div>
-          <Feed
-            className={styles.chatArea}
-            style={{
-              height: `calc(${defaultChatAreaHeight} - ${
-                this.state.heightDiff
-              }px)`
-            }}
-          >
-            {reverse(this.props.chatMessages).map(message => (
-              <Feed.Event key={message.id}>
-                <Feed.Label
-                  icon={
-                    message.user.id === this.props.user.id
-                      ? "user"
-                      : "user outline"
-                  }
-                />
-                <Feed.Content>{message.message}</Feed.Content>
-                <Feed.Date>{message.createdAt}</Feed.Date>
-              </Feed.Event>
-            ))}
-          </Feed>
-          <ChatSendForm {...this.props} />
-        </div>
-      </div>
+      <Chat
+        user={this.props.user}
+        title="ラウンジチャット"
+        chatMessages={this.props.chatMessages}
+        handleSubmit={this.props.presenter.handleSubmitChatMessage}
+      >
+        hello
+      </Chat>
     );
   }
 }
