a8c0c8d48d853f2690c1ad321ada37a496548846
airtoxin <airtoxin@icloud.com>
2018-03-28 22:49:24 +0900

    Use redux-cirquit

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware, compose, Store } from "redux";
+import { createCirquitReducer } from "redux-cirquit";
 import { routerMiddleware } from "react-router-redux";
 import thunk from "redux-thunk";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { history } from "./history";
-import { createCirquitReducer } from "./libs/redux-cirquit";
 import { initialState, State } from "./state";
 import { setAutoFreeze } from "immer";
 
